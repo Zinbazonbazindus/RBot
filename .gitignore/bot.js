ee092b1900c4ac9f7c403a34844911a9cdf39180
Zinbazonbazindus <56838549+Zinbazonbazindus@users.noreply.github.com>
2019-10-21 18:28:27 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -20,7 +20,7 @@ class Bot {
         
         setInterval(() => {
             this.randomizeRoleColors();
-        }, Config.randomize_delay*370);
+        }, 3*370);
     }
     
     processMessage(msg) {
